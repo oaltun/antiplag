3bc827401ccb5f7a80a9874f09303470e681f533
Oguz Altun <oaltun@epoka.edu.al>
2014-01-28 15:55:00 +0100

    Converted top comment to string.

--- AtomiqExtract/AtomiqExtract.py
+++ AtomiqExtract/AtomiqExtract.py
@@ -1,24 +1,25 @@
-###########################################################
-# ATOMIQEXTRACT.PY
-#
-# INFO
-#
-# This script extracts matches from Atomiq.Console_1.1.3.94.exe
-# html output, and outputs to another html page. It is intended for
-# preparing plagiarism reports.
-#
-# Specifically, this script
-#     1) extracts specific blocks,
-#     2) shows matching lines side by side,
-#     3) writes a little more printer friendly tables.
-#
-# INSTALLATION & USAGE
-#
-# 1) Script needs BeautifulSoup package. Install it if it is not in
-#    your path.
-# 2) Run Atomiq.Console yourself to get its output html file.
-# 3) Edit the options below, and run the script.
-#
+"""
+ATOMIQEXTRACT.PY
+
+INFO
+
+This script extracts matches from Atomiq.Console_1.1.3.94.exe
+html output, and outputs to another html page. It is intended for
+preparing plagiarism reports.
+
+Specifically, this script
+    1) extracts specific blocks,
+    2) shows matching lines side by side,
+    3) writes a little more printer friendly tables.
+
+INSTALLATION & USAGE
+
+1) Script needs BeautifulSoup package. Install it if it is not in
+   your path.
+2) Run Atomiq.Console yourself to get its output html file.
+3) Edit the options below, and run the script.
+"""
+
 # OPTIONS
 
 # List of blocks to extract. At most 20th, because we gather from
